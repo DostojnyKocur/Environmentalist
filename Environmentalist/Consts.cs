3980ff7d5c41fb6befec4aa6f2071acb5d90fbd1
Błażej Brzeżniak <blazej.brzezniak@gmail.com>
2020-05-21 21:19:17 +0200

    Update consts

--- Environmentalist/Consts.cs
+++ Environmentalist/Consts.cs
@@ -2,7 +2,8 @@
 {
     public static class Consts
     {
-        public const uint EntropySize = 1024;
+        public const uint EntropySize = 64;
+        public const string EntropyFileSuffix = "_entropy";
 
         public const string EnvironmentalVariableTagName = "[EnvVar]";
         public const string KeePassTagName = "[KeePass]";
