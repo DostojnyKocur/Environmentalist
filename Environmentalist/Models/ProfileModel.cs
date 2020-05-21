3b84ee4338ad43a61de68d7c5f7aad8a7f11edd7
Błażej Brzeżniak <blazej.brzezniak@gmail.com>
2020-05-21 21:17:24 +0200

    Use process file helper in profile reader

--- Environmentalist/Models/ProfileModel.cs
+++ Environmentalist/Models/ProfileModel.cs
@@ -4,6 +4,9 @@ namespace Environmentalist.Models
 {
 	public sealed class ProfileModel
 	{
+		public ProfileModel() { }
+		public ProfileModel(Dictionary<string, string> initialData) => Fields = initialData;
+
 		public Dictionary<string, string> Fields { get; } = new Dictionary<string, string>();
 	}
 }
